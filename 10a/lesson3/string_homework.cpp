c29b4f294e04ea304ee45e06c3932f9824e3baec
VIKIPLAYBG <viktorsirakov08@gmail.com>
2025-03-09 14:50:13 +0200

    HW 2 Progress

--- 10a/lesson3/string_homework.cpp
+++ 10a/lesson3/string_homework.cpp
@@ -12,48 +12,76 @@ public:
      * @brief Construct a new String object and it should have a default value of ""
      *
      */
-    String();
+    String() {
+        size = 0;
+        data = new char[size + 1];
+        data[0] = '\0';
+    }
 
     /**
      * @brief Construct a new String object with the provided value
      *
      * @param str
      */
-    String(const char *str);
+    String(const char *str) {
+        size = strlen(str);
+        data = new char[size + 1];
+        strcpy(data, str);
+    }
 
     /**
      * @brief Destroy the String object
      *
      */
-    ~String();
+    ~String() {
+        delete[] data;
+        size = 0;
+    }
 
     /**
      * @brief Returns the string as a C-style string - char*
      *
      * @return const char*
      */
-    char *toCString() const;
+    char *toCString() const {
+        return data;
+    }
 
     /**
      * @brief Returns the length of the string
      *
      * @return size_t
      */
-    size_t length() const;
+    size_t length() const {
+        size_t len = 0;
+        for(size_t i = 0; data[i] != '\0'; i++)
+            len++;
+        return len;
+    }
 
     /**
      * @brief Appends a character to the string
      *
      * @param ch
      */
-    void append(char ch);
+    void append(char ch) {
+        data[size++] = ch;
+        if(ch != '\0')
+            data[size] = '\0';
+        else
+            data[size++] = ch;
+    }
 
     /**
      * @brief Append the provided string to the current string
      *
      * @param str
      */
-    void concat(const char *str);
+    void concat(const char *str) {
+        for(int i = 0; str[i] != '\0'; i++) {
+            append(str[i]);
+        }
+    }
 
     /**
      * @brief Compares 2 strings
@@ -61,5 +89,15 @@ public:
      * @param other
      * @return int 0 if the strings are equal, 1 if the current string is greater, -1 if the current string is smaller
      */
-    int compare(const String &other) const;
+    int compare(const String &other) const {
+        size_t i = 0;
+        while(data[i] != '\0' && other.data[i] != '\0') {
+            if(data[i] < other.data[i])
+                return -1;
+            if(data[i] > other.data[i])
+                return 1;
+            i++;
+        }
+        return 0;
+    }
 };
