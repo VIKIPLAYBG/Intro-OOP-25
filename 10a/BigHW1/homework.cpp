15d99e8360f355000d67a2736c2e8d6ee45744ee
VIKIPLAYBG <viktorsirakov08@gmail.com>
2025-05-06 15:11:20 +0300

    Lesson 7, 8 and Big HW 1

---
+++ 10a/BigHW1/homework.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Operation {
+
+    string name;
+    string symbol;
+
+public:
+    //Para Constructor
+    //Copy Constructor
+    //Operator=
+    //Fully Virtual method - execute(const double n1, const double n2)
+    //All Getters
+    //All Setters
+    
+};
