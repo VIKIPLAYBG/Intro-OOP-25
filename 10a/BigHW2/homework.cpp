73efe29032ff5e886d52d925037b53a16f4ab4d9
VIKIPLAYBG <viktorsirakov08@gmail.com>
2025-06-08 00:29:45 +0300

    Big HW 2 - Had to start over, but I'm almost done

--- 10a/BigHW2/homework.cpp
+++ 10a/BigHW2/homework.cpp
@@ -1,100 +1,117 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <sstream>
-#include <string>
 #include <vector>
 
 using namespace std;
 
 class EmployeeSalaries {
 
-    double* salaries; //Must be positive, from 910.00 to 2500.00
+    double* empSalaries; //positive, between 910 and 2500
     size_t size;
     size_t capacity;
-    string employeeName;
+    string empName;
+
+    void resize()
+    {
+        this->capacity *= 2;
+        double *tmp = new double[this->capacity];
+        for (size_t i = 0; i < this->size; i++)
+        {
+            tmp[i] = this->empSalaries[i];
+        }
+        delete[] this->empSalaries;
+        this->empSalaries = tmp;
+    }
 
 public:
-
     EmployeeSalaries() {
-        capacity = 1;
-        salaries = new double[capacity];
-        employeeName = "";
+        this->size = 1;
+        this->capacity = 2;
+        this->empSalaries = new double[size];
+        this->empSalaries[0] = 910;
+        this->empName = "";
     }
 
-    EmployeeSalaries(double* salaries, size_t size, size_t capacity, string employeeName) {
-        this->salaries = new double[capacity];
-        for(size_t i = 0; i < capacity; i++) {
-            if(salaries[i] < 910 || salaries[i] > 2500)
-                throw invalid_argument("(Employee Para Constructor) Invalid Salary!");
-            this->salaries[i] = salaries[i];
-        }
+    EmployeeSalaries(double* empSalaries, size_t size, size_t capacity, string empName) {
         this->size = size;
         this->capacity = capacity;
-        this->employeeName = employeeName;
+        this->empName = empName;
+        this->empSalaries = new double[this->capacity];
+        for(size_t i = 0; i < size; i++) {
+            if(empSalaries[i] < 910 || empSalaries[i] > 2500)
+                throw invalid_argument("(EmployeeSalaries Para Constructor) Salary is invalid!");
+            this->empSalaries[i] = empSalaries[i];
+        }
     }
 
-    EmployeeSalaries(EmployeeSalaries &other) {
-        this->salaries = new double[other.capacity];
-        for(size_t i = 0; i < other.capacity; i++) {
-            if(other.salaries[i] < 910 || other.salaries[i] > 2500)
-                throw invalid_argument("(Employee Copy Constructor) Invalid Salary!");
-            this->salaries[i] = other.salaries[i];
-        }
+    EmployeeSalaries(const EmployeeSalaries &other) {
         this->size = other.size;
         this->capacity = other.capacity;
-        this->employeeName = other.employeeName;
+        this->empName = other.empName;
+        this->empSalaries = new double[this->capacity];
+        for(size_t i = 0; i < this->size; i++) {
+            this->empSalaries[i] = other.empSalaries[i];
+        }
     }
 
-    EmployeeSalaries &operator=(EmployeeSalaries &other) {
+    EmployeeSalaries& operator=(const EmployeeSalaries& other) {
         if(this != &other) {
-            this->salaries = new double[other.capacity];
-            for(size_t i = 0; i < other.capacity; i++) {
-                if(other.salaries[i] < 910 || other.salaries[i] > 2500)
-                    throw invalid_argument("(Employee Operator=) Invalid Salary!");
-                this->salaries[i] = other.salaries[i];
-            }
             this->size = other.size;
             this->capacity = other.capacity;
-            this->employeeName = other.employeeName;
+            this->empName = other.empName;
+            delete[] this->empSalaries;
+            this->empSalaries = new double[this->capacity];
+            for(size_t i = 0; i < this->size; i++) {
+                this->empSalaries[i] = other.empSalaries[i];
+            }
         }
         return *this;
     }
 
-    size_t getSize() {
+    ~EmployeeSalaries() {
+        delete[] this->empSalaries;
+    }
+
+    double getAvgSalary() {
+        double sum = 0;
+        for(int i = 0; i < this->size; i++) {
+            sum += empSalaries[i];
+        }
+        sum /= this->size;
+        return sum;
+    }
+
+    void addSalary(double salary) {
+        if(this->size == this->capacity)
+            resize();
+        if(salary < 910 || salary > 2500)
+            throw invalid_argument("(AddSalary) Salary is invalid!");
+        this->empSalaries[size++] = salary;
+    }
+
+    size_t getSize() const {
         return this->size;
     }
 
-    size_t getCapacity() {
+    size_t getCapacity() const {
         return this->capacity;
     }
 
-    string getName() {
-        return this->employeeName;
+    string getName() const {
+        return this->empName;
     }
 
-    void resize() {
-        this->capacity *= 2;
-        double* tmp = new double[this->capacity];
-        for(size_t i = 0; i < this->size; i++) {
-            tmp[i] = salaries[i];
-        }
-        delete[] salaries;
-        salaries = tmp;
-    }
-
-    void addSalary(double newSalary) {
-        if(this->size == this->capacity)
-            resize();
-        salaries[this->size++] = newSalary;
-    }
 };
 
 class Person {
 
 protected:
-    string name; //Mustn't be empty
-    int age; //Must be positive and lower than 65
-    static int numOfPeople;
+
+    string name; //Cannot be empty
+    int age; //Between 0 and 65
+    static int count;
 
 public:
 
@@ -105,240 +122,279 @@ public:
         if(age < 0 || age > 65)
             throw invalid_argument("(Person Para Constructor) Age is invalid!");
         this->age = age;
-        this->numOfPeople++;
+        count++;
     }
 
     Person(const Person &other) {
-        if(other.name.empty())
-            throw invalid_argument("(Person Copy Constructor) Name is empty!");
         this->name = other.name;
-        if(other.age < 0 || other.age > 65)
-            throw invalid_argument("(Person Copy Constructor) Age is invalid!");
         this->age = other.age;
-        this->numOfPeople++;
+        count++;
     }
+
+    virtual ~Person() {}
 
     Person &operator=(const Person &other) {
         if(this != &other) {
-            if(other.name.empty())
-                throw invalid_argument("(Person Operator=) Name is empty!");
             this->name = other.name;
-            if(other.age < 0 || other.age > 65)
-                throw invalid_argument("(Person Operator=) Age is invalid!");
             this->age = other.age;
         }
         return *this;
     }
 
-    string getName() {
-        return this->name;
+    string getName() const {
+        return name;
     }
 
-    int getAge() {
-        return this->age; 
+    int getAge() const {
+        return age;
     }
 
-    virtual int getWorkers() {
-        return this->numOfPeople;
+    static int getCount() {
+        return count;
     }
+
+    virtual char getType() const = 0;
 };
 
-int Person::numOfPeople = 0;
+int Person::count = 0;
 
 class Employee : public Person {
-    
-    EmployeeSalaries* empSalaries;
-    string position;
-    int experience;
-    static int numOfEmployees;
+
+    string position; //Cannot be empty
+    int experience; //1 or greater
+    EmployeeSalaries salaries; //All salaries must be valid
+    static int eCount;
 
 public:
-    Employee(EmployeeSalaries* empSalaries, string position, int experience) : Person(name, age) {
-        for(size_t i = 0; i < empSalaries->getSize(); i++)
-            this->empSalaries[i] = empSalaries[i];
+
+    Employee(string name, int age, string position, int experience) : Person(name, age) {
         if(position.empty())
             throw invalid_argument("(Employee Para Constructor) Position is empty!");
         this->position = position;
         if(experience < 1)
             throw invalid_argument("(Employee Para Constructor) Experience is invalid!");
         this->experience = experience;
-        this->numOfEmployees++;
+        eCount++;
     }
 
     Employee(const Employee &other) : Person(other) {
-        for(size_t i = 0; i < other.empSalaries->getSize(); i++)
-            this->empSalaries[i] = other.empSalaries[i];
-        if(other.position.empty())
-            throw invalid_argument("(Employee Copy Constructor) Position is empty!");
         this->position = other.position;
-        if(other.experience < 1)
-            throw invalid_argument("(Employee Copy Constructor) Experience is invalid!");
         this->experience = other.experience;
-        this->numOfEmployees++;
+        eCount++;
     }
+
+    virtual ~Employee() {}
 
     Employee &operator=(const Employee &other) {
         if(this != &other) {
-            for(size_t i = 0; i < other.empSalaries->getSize(); i++)
-                this->empSalaries[i] = other.empSalaries[i];
-            if(other.position.empty())
-                throw invalid_argument("(Employee Operator=) Position is empty!");
+            Person::operator=(other);
             this->position = other.position;
-            if(other.experience < 1)
-                throw invalid_argument("(Employee Operator=) Experience is invalid!");
             this->experience = other.experience;
         }
         return *this;
     }
 
-    string getPos() {
-        return position;
+    string getPos() const {
+        return this->position;
     }
 
-    int getExp() {
-        return experience;
+    int getExp() const {
+        return this->experience;
+    }
+
+    double calculateAverageSalary() {
+        return this->salaries.getAvgSalary();
     }
 
     void addSalary(double salary) {
-        empSalaries->addSalary(salary);
+        this->salaries.addSalary(salary);
     }
 
-    int getWorkers() override {
-        return numOfEmployees;
+    static int getCount() {
+        return eCount;
+    }
+
+    friend istream &operator>>(istream& is, Employee &emp) {
+        string line;
+        getline(is, line);
+
+        stringstream ss(line);
+        string strName, strAge, strPos, strExp;
+
+        getline(ss, strName);
+        getline(ss, strAge);
+        getline(ss, strPos);
+        getline(ss, strExp);
+
+        stringstream ssName(strName), ssAge(strAge), ssPos(strPos), ssExp(strExp);
+
+        string name, position;
+        int age, experience;
+        ssName >> name;
+        ssAge >> age;
+        ssPos >> position;
+        ssExp >> experience;
+
+        emp.name = name;
+        emp.age = age;
+        emp.position = position;
+        emp.experience = experience;
+
+        return is;
+    }
+
+    friend ostream &operator<<(ostream& os, const Employee &emp) {
+        os << "E " << emp.getName() << " " << emp.getAge() << " " << emp.getPos() << " " << emp.getExp() << endl;
+
+        return os;
+    }
+
+    char getType() const override {
+        return 'E';
     }
 };
 
-int Employee::numOfEmployees = 0;
+int Employee::eCount = 0;
 
 class Manager : public Person {
-    string department; //Can be "delivery", "marketing", "sales", or "human resources"
-    static int numOfManagers;
+
+    string department; //"delivery", "marketing", "sales" or "human resources"
+    static int mCount;
 
 public:
-    
-    Manager(string department) : Person(name, age) {
-        if(department != "delivery" || department != "marketing" || department != "sales" || department != "human resources")
+
+    Manager(string name, int age, string department) : Person(name, age) {
+        if(department != "Delivery" && department != "Marketing" && department != "Sales" && department != "Human Resources")
             throw invalid_argument("(Manager Para Constructor) Department is invalid!");
         this->department = department;
-        this->numOfManagers++;
+        mCount++;
     }
 
     Manager(const Manager &other) : Person(other) {
-        if(other.department != "delivery" || other.department != "marketing" || other.department != "sales" || other.department != "human resources")
-            throw invalid_argument("(Manager Copy Constructor) Department is invalid!");
         this->department = other.department;
-        this->numOfManagers++;
+        mCount++;
     }
 
     Manager &operator=(const Manager &other) {
         if(this != &other) {
             Person::operator=(other);
-            if(other.department != "delivery" || other.department != "marketing" || other.department != "sales" || other.department != "human resources")
-                throw invalid_argument("(Manager Operator=) Department is invalid!");
             this->department = other.department;
         }
         return *this;
     }
 
-    string getDept() {
+    string getDept() const {
         return this->department;
     }
 
-    int getWorkers() override {
-        return numOfManagers;
+    static int getCount() {
+        return mCount;
     }
 
-    void payEmployeeByName(string empName, vector<Employee> &employees, double salary) {
-        for(auto employee : employees) {
-            if(empName == employee.getName()) {
-                employee.addSalary(salary);
-                break;
-            }
-        }
-        throw invalid_argument("(Manager Pay Employee) Employee not found!");
+    friend istream &operator>>(istream &is, Manager &mng) {
+        string line;
+        getline(is, line);
+
+        stringstream ss(line);
+        string strName, strAge, strDept;
+
+        getline(ss, strName);
+        getline(ss, strAge);
+        getline(ss, strDept);
+
+        stringstream ssName(strName), ssAge(strAge), ssDept(strDept);
+
+        string name, department;
+        int age;
+        ssName >> name;
+        ssAge >> age;
+        ssDept >> department;
+
+        mng.name = name;
+        mng.age = age;
+        mng.department = department;
+
+        return is;
+    }
+
+    friend ostream &operator<<(ostream &os, const Manager &mng) {
+        os << "M " << mng.getName() << " " << mng.getAge() << " " << mng.getDept() << endl;
+
+        return os;
+    }
+
+    char getType() const override {
+        return 'M';
     }
 };
 
-int Manager::numOfManagers = 0;
-
-ostream &operator<<(ostream &os, Person &person) {
-    os << person.getName() << ' ' << person.getAge();
-    return os;
-}
-
-istream &operator>>(istream &is, Person &person) {
-    string line;
-    getline(is, line);
-
-    stringstream ss(line);
-
-    string strName, strAge;
-    getline(ss, strName, ',');
-    getline(ss, strAge, ',');
-
-    stringstream ssName(strName), ssAge(strAge);
-
-    string name;
-    int age;
-
-    ssName >> name;
-    ssAge >> age;
-
-    if(name.empty())
-        throw invalid_argument("(FileManager Operator>>) Name is empty!");
-
-    if(age < 0 || age > 65)
-        throw invalid_argument("(FileManager Operator>>) Age is invalid!");
-
-    person = Person(name, age);
-    return is;
-}
+int Manager::mCount = 0;
 
 class FileManager {
 
     const string in_name, out_name;
 
 public:
-    FileManager(string in, string out) : in_name(in), out_name(out) {}
 
-    void read(vector<Person> people, string personName, int personAge) {
-        Person person(personName, personAge);
-        ifstream inFile(in_name);
-        if(!inFile)
-            throw runtime_error("(FileManager Read) Cannot open input file!");
+    FileManager(const string in_name, const string out_name) : in_name(in_name), out_name(out_name) {}
 
-        while (!inFile.eof()) {
-            inFile >> person;
-            people.push_back(person);
+    static void writeInFile(const vector<Person*> &people, string out_name) {
+        ofstream out(out_name);
+        if(!out.is_open())
+            throw runtime_error("(FileManager Write) Output File cannot be opened");
+
+        for (Person* p : people) {
+            char type = p->getType();
+            if (type == 'E') {
+                Employee* e = dynamic_cast<Employee*>(p);
+                out << "E " << e->getName() << " " << e->getAge() << " " << e->getPos() << " " << e->getExp() << "\n";
+            }
+            else if (type == 'M') {
+                Manager* m = dynamic_cast<Manager*>(p);
+                out << "M " << m->getName() << " " << m->getAge() << " " << m->getDept() << "\n";
+            }
         }
-        inFile.close();
+
+        out.close();
     }
 
-    void write(const vector<Person> people) {
-        ofstream outFile(out_name);
-        if(!outFile)
-            throw runtime_error("(FileManager Write) Cannot open output file!");
+    static std::vector<Person*> readFromFile(const std::string& filename) {
+        std::vector<Person*> result;
+        std::ifstream in(filename);
+        if (!in.is_open()) throw std::runtime_error("Cannot open file for reading!");
 
-        for(auto p: people) {
-            outFile << p << endl;
+        std::string line;
+        while (getline(in, line)) {
+            std::istringstream iss(line);
+            char type;
+            iss >> type;
+            if (type == 'E') {
+                std::string name, pos;
+                int age, exp;
+                iss >> name >> age >> pos >> exp;
+                result.push_back(new Employee(name, age, pos, exp));
+            }
+            else if (type == 'M') {
+                std::string name, dept;
+                int age;
+                iss >> name >> age >> dept;
+                result.push_back(new Manager(name, age, dept));
+            }
         }
-        outFile.close();
+
+        in.close();
+        return result;
     }
 };
 
 int main() {
-    try {
-        vector<Person> people;
-        FileManager manager("read.txt", "write.txt");
-        Person person("Antonio Slavkov", 32);
-        manager.read(people, "Antonio Slavkov", 32);
-        for(auto p : people) {
-            cout << p << endl;
-        }
-        manager.write(people);
-    } catch(exception &e) {
-        cerr << "Error!" << e.what() << endl;
-    }
+
+    FileManager fileman("read.txt", "write.txt");
+    vector<Person*> people = fileman.readFromFile("read.txt");
+    fileman.writeInFile(people, "write.txt");
+
+    for(auto p : people)
+        delete p;
 
     return 0;
 }
